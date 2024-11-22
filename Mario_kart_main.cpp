a5dc78f8594ab699b2846a29d7bd8f7d2f1bba3d
RampantBag43488 <inakimancera@gmail.com>
2024-11-22 10:43:42 -0600

    Update Mario_kart_main.cpp

--- Mario_kart_main.cpp
+++ Mario_kart_main.cpp
@@ -1,48 +1,42 @@
 #include <iostream>
 using namespace std;
-#include "Mario_kart.h"
+#include "Personajes.h"
+#include "Carcasa.h"
+#include "Llantas.h"
 
 int main() {
     // Creación de objetos
-    Personaje mario(10.0f, 5.0f, 70.0f);
-    Coche kart(150.0f, 30.0f, 500.0f);
-    Llantas llantas1(40.0f, 20.0f, 50.0f);
+    Personaje personaje();
+    Carcasa carcasa();
+    Llantas llantas();
 
-    // Mostrar valores iniciales
-    cout << "Personaje Mario: Velocidad = " << mario.getVelocidad_p() 
-         << ", Aceleracion = " << mario.getAceleracion_p() 
-         << ", Peso = " << mario.getPeso_p() << endl;
 
-    cout << "Coche Kart: Velocidad = " << kart.getVelocidad_co() 
-         << ", Aceleracion = " << kart.getAceleracion_co() 
-         << ", Peso = " << kart.getPeso_co() << endl;
+    personaje.setVelocidad(12.5f);
+    personaje.setAceleracion(6.0f);
+    personaje.setPeso(72.0f);
+    carcasa.setVelocidad_(160.0f);
+    carcasa.setAceleracion_(35.0f);
+    carcasa.setPeso(520.0f);
+    carcasa.setEstilo(0)
+    llantas.setVelocidad(45.0f);
+    llantas.setAceleracion(25.0f);
+    llantas.setPeso(55.0f);
+    llantas.setTipo(1);
 
-    cout << "Llantas1: Velocidad = " << llantas1.getVelocidadll() 
-         << ", Aceleracion = " << llantas1.getAceleracionll() 
-         << ", Peso = " << llantas1.getPesoll() << endl;
 
-    // Modificar valores con setters
-    mario.setVelocidad_p(12.5f);
-    mario.setAceleracion_p(6.0f);
-    mario.setPeso_p(72.0f);
-    kart.setVelocidad_co(160.0f);
-    kart.setAceleracion_co(35.0f);
-    kart.setPeso_co(520.0f);
-    llantas1.setVelocidadll(45.0f);
-    llantas1.setAceleracionll(25.0f);
-    llantas1.setPesoll(55.0f);
 
-    cout << "Personaje Mario: Velocidad = " << mario.getVelocidad_p() 
-         << ", Aceleracion = " << mario.getAceleracion_p() 
-         << ", Peso = " << mario.getPeso_p() << endl;
+    cout <<"Personaje Mario: Velocidad = " << personaje.getVelocidad() 
+         << ", Aceleracion = " << personaje.getAceleracion() 
+         << ", Peso = " << personaje.getPeso()
+         << ", Nombre = " << personaje.getNombre() << endl;
 
-    cout << "Coche Kart: Velocidad = " << kart.getVelocidad_co() 
-         << ", Aceleracion = " << kart.getAceleracion_co() 
-         << ", Peso = " << kart.getPeso_co() << endl;
+    cout << "Carcasa: Velocidad = " << carcasa.getVelocidad() 
+         << ", Aceleracion = " << carcasa.getAceleracion() 
+         << ", Peso = " << carcasa.getPeso()
+         << ", Estilo = " << carcasa.getEstilo() << endl;
 
-    cout << "Llantas1: Velocidad = " << llantas1.getVelocidadll() 
-         << ", Aceleracion = " << llantas1.getAceleracionll() 
-         << ", Peso = " << llantas1.getPesoll() << endl;
-
-    return 0;
+    cout << "Llantitas de hule: Velocidad = " << llantas.getVelocidad() 
+         << ", Aceleracion = " << llantas.getAceleracion() 
+         << ", Peso = " << llantas.getPeso()
+         << ", Tipo = " << llantas.getTipo() << endl;
 }
