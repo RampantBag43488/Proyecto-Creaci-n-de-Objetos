6d14b415a5714eef0c05a812d21bc8b1332ab0a4
RampantBag43488 <inakimancera@gmail.com>
2024-11-28 09:49:28 -0600

    Update Mario_kart_main.cpp Menu

--- Mario_kart_main.cpp
+++ Mario_kart_main.cpp
@@ -1,42 +1,141 @@
 #include <iostream>
 using namespace std;
-#include "Personajes.h"
-#include "Carcasa.h"
-#include "Llantas.h"
+#include "Kart.h"
+
+//Menu
+void menu(){
+
+    //Imprime las opciones que va a tener el sistema
+    cout << "Menu:\n";
+    cout << "1. Mostrar detalles del kart. \n";
+    cout << "2. Cambiar aceleracion de la carcasa. \n";
+    cout << "3. Cambiar peso de la carcasa. \n";
+    cout << "4. Cambiar velocidad de la carcasas. \n";
+    cout << "5. Cambiar estilo de la carcasa. \n";
+    cout << "6. Cambiar aceleracion de las llantas. \n";
+    cout << "7. Cambiar peso de las llantas. \n";
+    cout << "8. Cambiar velocidad de las llantas. \n";
+    cout << "9. Cambiar el tipo de llantas. \n(Ponga: 0 = todoterreno/1 = pequeñas)\n";
+    cout << "10. Cambiar aceleracion del personaje. \n";
+    cout << "11. Cambiar peso del personaje. \n";
+    cout << "12. Cambiar velocidad del personaje. \n";
+    cout << "13. Cambiar nombre del personaje. \n";
+    cout << "14. Mostrar detalles de las partes. \n";
+    cout << "15. Salir \n";
+}
 
 int main() {
-    // Creación de objetos
-    Personaje personaje();
-    Carcasa carcasa();
-    Llantas llantas();
+     Kart kart1(0, 1, "Luigi");
+     int opcion = 0;
+     float tem_acel = 0.0, tem_veloc = 0.0, tem_peso = 0.0;
+     bool tem_estilo = 0, tem_tipo = 0;
+     string tem_nombre;
 
 
-    personaje.setVelocidad(12.5f);
-    personaje.setAceleracion(6.0f);
-    personaje.setPeso(72.0f);
-    carcasa.setVelocidad_(160.0f);
-    carcasa.setAceleracion_(35.0f);
-    carcasa.setPeso(520.0f);
-    carcasa.setEstilo(0)
-    llantas.setVelocidad(45.0f);
-    llantas.setAceleracion(25.0f);
-    llantas.setPeso(55.0f);
-    llantas.setTipo(1);
+     while(opcion < 15 && opcion > -1){
 
+  		//Impresion de menu
+  		menu();
+  		//Leer indice que selecciona la opcion del menu
+  		cin >> opcion;
 
+  		//Switch donde dependiendo la eleccion efectua un diferente procedimiento
+  		switch(opcion){
 
-    cout <<"Personaje Mario: Velocidad = " << personaje.getVelocidad() 
-         << ", Aceleracion = " << personaje.getAceleracion() 
-         << ", Peso = " << personaje.getPeso()
-         << ", Nombre = " << personaje.getNombre() << endl;
+  			//Caso 1 que imprime las estadisticas del kart y sus detalles
+  			case 1:
+  				kart1.mostrarDetalles();
+  			  break;
 
-    cout << "Carcasa: Velocidad = " << carcasa.getVelocidad() 
-         << ", Aceleracion = " << carcasa.getAceleracion() 
-         << ", Peso = " << carcasa.getPeso()
-         << ", Estilo = " << carcasa.getEstilo() << endl;
+  			//Caso 2 cambia la aceleracion de la carcasa
+  			case 2:
+                    cout << "Dame la aceleracion de la carcasa: ";
+                    cin >> tem_acel;
+  				kart1.setCarcasaAceleracion(tem_acel);
+  			  break;
 
-    cout << "Llantitas de hule: Velocidad = " << llantas.getVelocidad() 
-         << ", Aceleracion = " << llantas.getAceleracion() 
-         << ", Peso = " << llantas.getPeso()
-         << ", Tipo = " << llantas.getTipo() << endl;
+  			//Caso 3 cambia el peso de la carcasa
+  			case 3:
+                    cout << "Dame el peso de la carcasa: ";
+                    cin >> tem_peso;
+  				kart1.setCarcasaPeso(tem_peso);
+  			  break;
+
+  			//Caso 4 cambiar la velocidad de la carcasa
+  			case 4:
+                    cout << "Dame la velocidad de la carcasa: ";
+                    cin >> tem_veloc;
+  				kart1.setCarcasaVelocidad(tem_veloc);
+  			  break;
+
+  			//Caso 5 cambiar el estilo de la carcasa
+  			case 5:
+                    cout << "Dame el estilo de la carcasa: \n(Ponga: 0 para coche/1 para moto)\n";
+                    cin >> tem_estilo;
+  				kart1.setCarcasaEstilo(tem_estilo);
+   			  break;
+
+  			//Caso 6 cambia la aceleracion de las llantas
+  			case 6:
+                    cout << "Dame la aceleracion de las llantas: ";
+                    cin >> tem_acel;
+  				kart1.setLlantasAceleracion(tem_acel);
+  		  	break;
+
+  			//Caso 7 cambia el peso de las llantas
+  			case 7:
+                    cout << "Dame el peso de las llantas: ";
+                    cin >> tem_peso;
+  				kart1.setLlantasPeso(tem_peso);
+  			  break;
+
+  			//Caso 8 cambia la velocidad de las llantas
+  			case 8:
+                    cout << "Dame la velocidad de las llantas: ";
+                    cin >> tem_veloc;
+  				kart1.setLlantasVelocidad(tem_veloc);
+  			  break;
+
+               //Caso 9 cambia el tipo de llantas
+  			case 9:
+  				cout<< "Dame el tipo de llantas: \n(Ponga: 0 para todoterreno/1 para pequeñas)\n" ;
+  				cin >> tem_tipo;
+  				kart1.setLlantasTipo(tem_tipo);
+  			  break;
+
+               //Caso 10 cambia la aceleracion del personaje
+  			case 10:
+  				cout<< "Dame la aceleracion del pesonaje: " ;
+  				cin >> tem_acel;
+  				kart1.setPersonajeAceleracion(tem_acel);
+  			  break;
+
+               //Caso 11 cambia el peso del personaje
+               case 11:
+  				cout<< "Dame el peso del pesonaje: " ;
+  				cin >> tem_peso;
+  				kart1.setPersonajePeso(tem_peso);
+  			  break;
+
+               //Caso 12 cambia la velocidad del personaje
+               case 12:
+  				cout<< "Dame la velocidad del pesonaje: " ;
+  				cin >> tem_veloc;
+  				kart1.setPersonajeVelocidad(tem_veloc);
+  			  break;
+
+               //Caso 13 cambia el nombre del personaje
+               case 13:
+  				cout<< "Dame el nombre del pesonaje: " ;
+  				cin >> tem_nombre;
+  				kart1.setPersonajeNombre(tem_nombre);
+  			  break;
+
+  			case 14:
+  				kart1.mostrarDetallesPartes();
+  			  break;
+  		}
+     }
+     return 0;
 }
+     
