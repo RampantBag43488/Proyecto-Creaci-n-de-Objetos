71307bffe673b5d96947b4eee959d1fd943b1bc2
RampantBag43488 <inakimancera@gmail.com>
2024-11-28 21:47:54 -0600

    Update Carcasa.h comentarios

--- Carcasa.h
+++ Carcasa.h
@@ -1,3 +1,17 @@
+/*
+ * Proyecto Mario Kart
+ * Inaki Mancera Llano
+ * A01708827
+ * 28/11/2024
+ */
+
+/*
+ * La clase Carcasa recibe las estadisticas basicas de la clase
+ * ParametrosBase y define una variable mas para saber el estilo
+ * de la carcasa del kart.
+*/
+
+
 #ifndef CARCASA_H
 #define CARCASA_H
 
@@ -5,14 +19,18 @@
 #include "ParametrosBase.h"
 using namespace std;
 
-
+//Clase carcasa que define si el kart es una moto o un coche
 class Carcasa: public ParametrosBase {
     private:
+    //Estilo de carcasa
     bool Estilo;
 
     public:
+    //Constructores
     Carcasa();
     Carcasa(bool a) : Estilo(a){}
+
+    //Getter y setter
     string getEstilo();
     void setEstilo(bool);
 };
@@ -21,6 +39,7 @@ Carcasa::Carcasa(){
     Estilo = 0;
 }
 
+//Se usa booleano para eviar errores de escritura
 string Carcasa::getEstilo(){
     if (Estilo == 0){
         return "Coche";
